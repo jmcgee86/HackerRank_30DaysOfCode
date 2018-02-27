d3bf2114a0ad479ddbbea65f354a336fc662d488
Jim McGee <jmcgee86@gmail.com>
2018-02-27 00:21:27 +0000

    never did day 0, here it is!

---
+++ day0_HelloWorld.js
@@ -0,0 +1,7 @@
+function processData(inputString) {
+    // This line of code prints the first line of output
+    console.log("Hello, World.");
+    var input = inputString;
+console.log(input);
+    // Write the second line of output that prints the contents of 'inputString' here.
+} 
