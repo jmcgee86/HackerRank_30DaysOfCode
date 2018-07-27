33a389f9bb13397072cf63cc5bf1c3f936490bb5
Jim McGee <jmcgee86@gmail.com>
2018-07-27 15:11:52 +0000

    add solution for Day 16: Exceptions - Strings to Ints

---
+++ Day16_Exceptions_StringsToInts.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+class Solution {
+
+    static void Main(String[] args) {
+        string S = Console.ReadLine();
+        try
+        {
+            int n = Int32.Parse(S);
+            Console.WriteLine(n);
+        }
+        catch(Exception e)
+        {
+            Console.WriteLine("Bad String");
+        }
+        return;
+    }
+}
